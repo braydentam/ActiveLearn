cf2692811933a6962ae2605db88f87f4bc5934d3
Brayden Tam <brayden.tam@gmail.com>
2021-03-27 10:56:07 -0700

    fixed bugs

--- activelearnfrontend/src/components/TaskBar Teacher/ToggleButtonsTeacher.js
+++ activelearnfrontend/src/components/TaskBar Teacher/ToggleButtonsTeacher.js
@@ -36,14 +36,14 @@ export class ToggleButtons extends Component {
                         <ToggleLink to="/dashboard">
                             <button className="btn-question-teacher" onClick={()=>this.toggleMe('question')}>Ask a Question  </button>
                         </ToggleLink>
-                        <a className = "pad" href="/pad.html" target="_blank">Collab notes</a>
+                        <a className = "Newpad" href="/pad.html" target="_blank">Collab notes</a>
                         <a className = "pad">
                         <input className = "file" type="file" name="file" id="filesubmit"/>
                         </a>
                         <ToggleLink to= "/">
                             <button className="btn-leave-teacher" onClick={()=>this.toggleMe('leave')}> Leave Room </button>
                         </ToggleLink>
-                        <span className="code">Code: {this.state.room_code}</span>
+                        <div className="code">Code: {this.state.room_code}</div>
                     </div>
                 </div>
             </div>
